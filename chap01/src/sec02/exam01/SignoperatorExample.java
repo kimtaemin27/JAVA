adc8d3808234c71ee4daee2842d067e7402e62c0
김태민 <ktm3628@naver.com>
2022-07-13 12:14:12 +0900

    day 01

---
+++ chap01/src/sec02/exam01/SignoperatorExample.java
@@ -0,0 +1,21 @@
+package sec02.exam01;
+
+public class SignoperatorExample {
+
+	public static void main(String[] args) {
+		
+		int x = -100;
+		int result1 =+x;
+		int result2 =-x;
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : " + result2);
+		
+		byte b = 100;
+		// byte result3 = -b;
+		// ^ byte 타입 값을 부호 연산하려면 int 타입값으로 바꿔줘야함
+		int result3 = -b;
+		System.out.println("result3 : " + result3);
+
+	}
+
+}
