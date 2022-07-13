87ccdc2bc6e12e7e7e35d8ca90c3688c7f4b9fc6
김태민 <ktm3628@naver.com>
2022-07-13 09:35:47 +0900

    day 01

---
+++ chap01/src/sec03/exam01/LongOperationExample.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+public class LongOperationExample {
+
+	public static void main(String[] args) {
+		
+		byte value1 = 10;
+		int value2 = 100;
+		long value3 = 1000L;
+		long reslut = value1 + value2 + value3;
+		System.out.println(reslut);
+
+	}
+
+}
