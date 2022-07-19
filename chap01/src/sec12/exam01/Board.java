f05f3543292d0b005fa4aabc32ab9e970cf520d7
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-07-19 11:05:24 +0900

    07-19

---
+++ chap01/src/sec12/exam01/Board.java
@@ -0,0 +1,15 @@
+package sec12.exam01;
+
+public class Board {
+	
+	String subject;
+	String contnet;
+	String writer;
+	
+	public Board(String subject, String content, String writer) {
+		this.subject = subject;
+		this.contnet = content;
+		this.writer = writer;
+	}
+
+}
