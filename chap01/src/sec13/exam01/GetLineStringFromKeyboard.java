24012545801b5b459976156e001c5a01d3e8a591
김태민 <ktm3628@naver.com>
2022-07-20 16:22:11 +0900

    07-20

---
+++ chap01/src/sec13/exam01/GetLineStringFromKeyboard.java
@@ -0,0 +1,26 @@
+package sec13.exam01;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class GetLineStringFromKeyboard {
+
+	public static void main(String[] args) throws Exception{
+		
+		InputStream is = System.in;
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);
+		
+		while(true) {
+			System.out.println("key on : ");
+			String lineStr = br.readLine();
+			if(lineStr.equals("q") || lineStr.equals("quit")) break;
+			System.out.println("input : " + lineStr);
+			System.out.println();
+		}
+
+	}
+
+}
